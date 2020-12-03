6e2a66920404037f86560f76cc8a07a716e14790
Samantha Chin <69879837+Samantha876@users.noreply.github.com>
2020-12-03 12:16:38 -0500

    Update newuser.php

--- newuser.php
+++ newuser.php
@@ -1,3 +1,9 @@
+<head>
+    <meta charset="utf-8">
+	<title>BugMe Issue Tracker</title>
+	<link href="issue.css" type="text/css" rel="stylesheet" />
+	<script src="user.js" type="text/javascript"></script>
+</head>
 <?php
 header('Access-Control-Allow-Origin: *');
 
@@ -27,6 +33,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $lastname= filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING);
     $password= filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
     $email= filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+
+    $stmt = $conn->prepare("INSERT INTO users (id,firstname,lastname,password,email,date_joined) VALUES (:firstname,:lastname,password_hash(:password, PASSWORD_DEFAULT),:email)");
+
+    $stmt->bindParam(":firstname", $firstname);
+    $stmt->bindParam(":lastname", $lastname);
+    $stmt->bindParam(":password", $password);
+    $stmt->bindParam(":email", $email);
+
+    $stmt->execute();
     ?>
 
 <?php endif; ?>
